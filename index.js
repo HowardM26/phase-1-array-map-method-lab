cdc5ed024d2f9a2e8e551d0ae997ec6ae80f68c1
+ <+>
2023-05-01 18:08:09 -0400

    map method

--- index.js
+++ index.js
@@ -11,6 +11,50 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  const newTutorials = tutorials.map(function(title) {
+        // console.log(title)
+        // title is a string
+        //we want to capitalize each word in title. 
+        //If title was an array of words what method could we use to capitalize each word.
+        // toUppercase would be used. we can also use the map method.
+        //split it into an individual array of words and then use map method.
+        const newArray = title.split(' ');
+        const newArray2 = newArray.map(function(word) {
+        
+          const firstLetter = word.charAt(0).toUpperCase();
+          const restOfWord = word.slice(1);
+          return firstLetter + restOfWord;
+        })
+        console.log(newArray2)
+        return newArray2.join(' ')
+  })
+  console.log(newTutorials)
+  return newTutorials
 }
+titleCased()
+// function titleCased(str) {
+// var strArr = str.split(' ');
+// var newArr = [];
+
+// for(var i = 0; i < strArr.length; i++){
+
+//   var FirstLetter = strArr[i].charAt(0).toUpperCase();
+//   var restOfWord = strArr[i].slice(1);
+
+//   newArr[i] = FirstLetter + restOfWord;
+// }
+//   return newArr.join(' ');
+// }
+
+
+// ======================== practice code
+
+const numbers = [
+  1, 2, 3, 4, 5, 6
+]
+
+const newNumbers = numbers.map(function(value) {
+return value + 2;
+})
+console.log(newNumbers)
